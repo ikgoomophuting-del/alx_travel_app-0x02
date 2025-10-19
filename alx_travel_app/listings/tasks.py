0e09416b7e3e8cd1e55868c1ffa2867b1aa21c63
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-10-19 13:45:56 +0200

    Create tasks.py

---
+++ alx_travel_app/listings/tasks.py
@@ -0,0 +1,10 @@
+from celery import shared_task
+from django.core.mail import send_mail
+from django.conf import settings
+
+@shared_task
+def send_payment_confirmation_email(email, booking_reference, amount, status):
+    subject = f"Payment {status}: {booking_reference}"
+    message = f"Your payment of {amount} ETB for booking {booking_reference} is {status.lower()}."
+    send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [email])
+    return f"Email sent to {email}"
