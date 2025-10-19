568a490f6c17eadff47a77961a82818ee79a412b
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-10-19 11:28:49 +0200

    Implement payment initialization and verification
    Added functions to initialize and verify payments using Chapa API.

--- listings/utils/payment_gateway.py
+++ listings/utils/payment_gateway.py
@@ -1,1 +1,26 @@
+import requests
+from django.conf import settings
+
+CHAPA_SECRET = "CHAPA_TEST_SECRET"
+
+def initialize_payment(reference, amount, email):
+    response = requests.post(
+        "https://api.chapa.co/v1/transaction/initialize",
+        headers={"Authorization": f"Bearer {CHAPA_SECRET}"},
+        data={
+            "amount": amount,
+            "currency": "ETB",
+            "email": email,
+            "tx_ref": reference,
+            "callback_url": "http://127.0.0.1:8000/listings/verify/",
+        },
+    )
+    return response.json()
+
+def verify_payment(reference):
+    response = requests.get(
+        f"https://api.chapa.co/v1/transaction/verify/{reference}",
+        headers={"Authorization": f"Bearer {CHAPA_SECRET}"},
+    )
+    return response.json()
 
