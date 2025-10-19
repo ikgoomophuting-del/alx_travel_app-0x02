7886ae5e288b8e2b6d6155694753f470a98f9d1a
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-10-19 10:56:25 +0200

    Define GraphQL schema with queries and mutations

--- alx_travel_app/schema.py
+++ alx_travel_app/schema.py
@@ -1,1 +1,11 @@
+import graphene
+from listings.schema import ListingQuery, PaymentMutation
+
+class Query(ListingQuery, graphene.ObjectType):
+    pass
+
+class Mutation(PaymentMutation, graphene.ObjectType):
+    pass
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
 
